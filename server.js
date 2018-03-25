0485ccbd58200575779d5e0409e0cec454685a42
GreenCord <steve@nevizen.com>
2018-03-25 09:06:59 -0400

    Express setup initial commit

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +10,14 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Connect to DB using Mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/boc_db'
+);
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, 'MongoDB connection error:'));
 
 // Send every request to the React app
 // Define any API routes before this runs
